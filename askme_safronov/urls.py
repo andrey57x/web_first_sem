33a0be27a56f94e9cc813103f59acd7a8e3856cb
Saf1k <57andreys57@gmail.com>
2025-04-09 17:31:17 +0300

    Update urls.py

--- askme_safronov/urls.py
+++ askme_safronov/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app import urls
+from django.urls import include, re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    re_path(r'^app/', include('app.urls')),
 ]
 
-urlpatterns.extend(urls.urlpatterns)
